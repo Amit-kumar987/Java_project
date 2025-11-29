77742403e53f9230679cdf99da7409362a379831
Amit-kumar987 <amitkumar21012003@gmail.com>
2025-11-29 19:24:56 +0530

    Delete main19.java

--- main19.java
+++
@@ -1,8 +0,0 @@
-import exam.Result;
-
-public class main19 {
-    public static void main(String[] args) {
-        Result r = new Result(101, "AMIT KUMAR", 99, 98, 90);
-        r.display();
-    }
-}

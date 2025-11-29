0dd3fcfe777c37c261a624946ca2747fce98079f
Amit-kumar987 <amitkumar21012003@gmail.com>
2025-11-29 19:10:12 +0530

    Add files via upload

---
+++ INTERMEDIATE LEVEL/student/Student.java
@@ -0,0 +1,11 @@
+package student;
+
+public class Student {
+    public int roll;
+    public String name;
+
+    public Student(int roll, String name) {
+        this.roll = roll;
+        this.name = name;
+    }
+}
